edf2a5fbf7f27b93ff6087dc4fabf8d083f1546e
OrvalaAM <orvalavala@gmail.com>
2022-04-23 17:38:04 +0700

    menambahkan API Movies

--- modul8_1302204105/Program.cs
+++ modul8_1302204105/Program.cs
@@ -21,23 +21,65 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+
+
+app.MapGet("/api/Movies", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    List<Movie> listFilm = new List<Movie> {
+        new Movie("The Shawshank Redemption", "Frank Darabont", new List<string>{"Tim Robbins", "Morgan Freeman"}, "Tied for the best movie I have ever seen"),
+        new Movie("The Godfather", "Francis Ford Coppola", new List<string>{"Marlon Brando", "Al Pacino"}, "An offer so good, I couldn't refuse"),
+        new Movie("The Dark Knight", "Christopher Nolan", new List<string>{"Christian Bale", "Heath Ledger"}, "The Batman of our dreams! So much more than a comic book movie")
+    };
+    /*Movie film1 = new Movie   (
+           "Tintin",
+           "Herge",
+           new List<string>(),
+           "FILM YANG MANTAP"
+       );
+    Movie film2 = new Movie(
+           "Naruto",
+           "Nasashi",
+           new List<string>(),
+           "EPIC BATTLE"
+       );
+    Movie film3 = new Movie(
+           "Conan",
+           "Aoyama",
+           new List<string>(),
+           "GAK GEDE GEDE"
+       );
+    listFilm.Add(film1);
+    listFilm.Add(film2);
+    listFilm.Add(film3);*/
+    return listFilm;
 })
-.WithName("GetWeatherForecast");
+.WithName("GetMovies");
+
+app.MapPost("/api/Movies", () =>
+{
+    
+})
+.WithName("PostMovies");
+
+app.MapGet("/api/Movies/{id}", () =>
+{
+    
+})
+.WithName("GetMoviesID");
+app.MapDelete("/api/Movies/{id}", () =>
+{
+
+})
+.WithName("DeleteMoviesID");
+
+/*app.MapGet("/test", () => "HELLO WORLD")
+.WithName("TestAja");*/
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+
+
+internal record Movie(string Title, string Director, List<string> Stars, string Description)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
 }
